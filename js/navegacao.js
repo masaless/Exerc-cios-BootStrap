942567cb70c3b5771865bc907e02e5a1bc6d9a11
masaless <ndkextreemo@gmail.com>
2021-03-08 21:49:31 -0300

    Formulário BootStrap

--- js/navegacao.js
+++ js/navegacao.js
@@ -3,7 +3,7 @@
         if (!hash) return
 
         const link = document.querySelector(`[wm-link='${hash}']`)
-        if(!link) return
+        if (!link) return
 
         const destino = document.querySelector('[wm-link-destino]')
 
@@ -12,7 +12,10 @@
             .then(resp => resp.text())
             .then(html => {
                 destino.innerHTML = html
-                eval(html.match(/\<script\>([\s\S]*)\<script\>/)[1])
+                const resultado = html.match(html.match(/\<script\>([\s\S]*)\<script\>/))
+                if (resultado && resultado.length >= 2) {
+                    eval(resultado[1])
+                }
             })
     }
 
@@ -33,7 +36,7 @@
     }
 
     window.onhashchange = e => navegarViaAjax(location.hash)
-    
+
     configurarLinks()
     navegacaoInicial()
 })()
